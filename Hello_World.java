6fd4ed9b45aec62ff1b20cd22f5875a582fc72c9
mayurnigade <mayurnigade10@gmail.com>
2022-06-12 12:24:46 +0530

    New trial for all

---
+++ Hello_World.java
@@ -0,0 +1,13 @@
+package CODE;
+
+public class Hello_World {
+
+	public static void main(String[] args) {
+		
+		System.out.println("HELLO TEJAS");
+		System.out.println("HELLO Group16");
+System.out.println("End of program");
+	System.out.println("Branch try");	
+	}
+
+}
---
+++ add.java
@@ -0,0 +1,20 @@
+package CODE;
+
+import java.util.Scanner;
+//Edited By Akshay Edited in gamma
+
+public class add {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the two no = ");
+		int n1 = sc.nextInt();
+		int n2 = sc.nextInt();
+		
+		System.out.println("add = " + (n1+n2));
+		System.out.println("mod = " + (n1%n2));
+		System.out.println("mul = " + (n1*n2);
+	}
+
+}
